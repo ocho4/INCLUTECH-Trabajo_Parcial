5de03eebfbc7bc06c3e6395a5da77504e8a8870c
ocho4 <you@example.com>
2025-10-05 10:24:41 -0500

    Codigo base del proyecto sin modificaciones

---
+++ src/main/java/com/upc/inclutechtrabajo_parcial/InclutechTrabajoParcialApplication.java
@@ -0,0 +1,13 @@
+package com.upc.inclutechtrabajo_parcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InclutechTrabajoParcialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InclutechTrabajoParcialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/inclutechtrabajo_parcial/InclutechTrabajoParcialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.inclutechtrabajo_parcial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InclutechTrabajoParcialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
