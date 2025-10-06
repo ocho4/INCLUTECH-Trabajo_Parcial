10500daef773e0badcceb73837d8cbff1e71c674
ocho4 <you@example.com>
2025-10-05 21:16:32 -0500

    HomeController añadido para el enlace principal y Dockerfile añadido

---
+++ src/main/java/com/upc/inclutechtrabajo_parcial/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.upc.inclutechtrabajo_parcial.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "INCLUTECH API - Trabajo Parcial está funcionando correctamente!";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "API Health Check - OK";
+    }
+}
